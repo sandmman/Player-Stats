6a3eeb0176fa3aae6d5f7e166334d85baaeef417
Aaron Liberatore <aaron.liberatore@gmail.com>
2016-02-01 00:13:05 -0500

    Corrects players names to those on wikipedia

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 import ssl
-
+import csv
 
 def createURL(player):
     url = "https://en.wikipedia.org/wiki/"
@@ -9,54 +9,57 @@ def createURL(player):
         url += x + "_"
     return url[0:-1]
 
-def printTable(data):
-        for table in data:
-            print "\n"
-            for row in table:
-                if len(row) != 0:
-                    print str(row).strip('[]')
 playerStats = []
 
+# Get player names to scrape
 f = open("players.txt")
 players = f.readlines()
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
-for player in players[435:]:
-    context = ssl._create_unverified_context()
+#Establish context and user-agent
+#opener = urllib2.build_opener()
+#opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+context = ssl._create_unverified_context()
+
+for player in players:
 
     p = player.strip().split(" ")
-    contenturl = createURL(p)
-    print contenturl
+    contentURL = createURL(p)
+
+    valid = True
 
     try:
-        url = urllib2.urlopen(contenturl,context=context)
-    except URLError as e:
-        print 'An error occured fetching %s \n %s' % (url, e.reason)
+        url = urllib2.urlopen(contentURL,context=context)
+    except urllib2.URLError as e:
+        print 'Error (%s) occured fetching %s' % (e.reason,contentURL)
+        valid = False
 
     soup = BeautifulSoup(url.read())
 
     try:
         rows = soup.find("table", attrs={'class':'wikitable'}).findChildren(['th', 'tr'])
     except AttributeError as e:
-        print 'No tables found, exiting'
+        print 'No tables found for %s' % (player)
+        valid = False
 
-    player = []
-    player.append([p]) # Append Name
-    # Get header data
-    for row in rows:
-        headers = row.findAll('th')
-        cols = row.findAll('td')
-        if len(headers) != 0:
-            cols = [e.text.strip().encode('ascii', 'replace') for e in headers]
-            if len(cols) != 0:
-                player.append([e for e in cols])
-        else:
-            cols = [e.text.strip().encode('ascii', 'replace') for e in cols]
-            if len(cols) != 0:
-                player.append([e for e in cols])
-    playerStats.append(player)
+    if(valid):
+        stats = []
+        stats.append([player]) # Append Name
+        # Get header data
+        for row in rows:
+            headers = row.findAll('th')
+            cols = row.findAll('td')
+            if len(headers) != 0:
+                cols = [e.text.strip().encode('ascii', 'replace') for e in headers]
+                if len(cols) != 0:
+                    stats.append([e for e in cols])
+            else:
+                cols = [e.text.strip().encode('ascii', 'replace') for e in cols]
+                if len(cols) != 0:
+                    stats.append([e for e in cols])
+        playerStats.append(stats)
 
-
-
-printTable(playerStats)
+# Out to csv file
+with open('playerStats.csv', 'wb') as fp:
+    a = csv.writer(fp, delimiter=',')
+    for p in playerStats:
+        a.writerows(p)
